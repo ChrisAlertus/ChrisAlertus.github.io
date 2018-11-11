5bd09002c9c9d20ced21d0a8b2de374ac405a50b
Chris Alert <alertdrive44@gmail.com>
2018-11-11 01:17:07 -0500

    Updated the index file: LaunchYardDetail class. Made a few dummy posts and an About Me page. Added a few pictures. Learned that absolute references are a MUST for web pages relative to position in output folder

--- publish.bat
+++ publish.bat
@@ -2,6 +2,6 @@ git add --all
 git commit -a -m %1
 git push -u origin pelican-src
 pelican content -o output -s pelicanconf.py
-ghp-import output -r origin2 -b master
+ghp-import output -r origin -b master
 git push -u origin master
 git checkout pelican-src
